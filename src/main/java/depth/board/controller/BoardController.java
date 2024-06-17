1ed364fea0b7d91e4eddcba1fadeb756b299a74d
HuniHuns <i690367@gmail.com>
2024-06-18 00:53:33 +0900

    [ADD] board 리스트, 삭제, 수정기능 추가

--- src/main/java/depth/board/controller/BoardController.java
+++ src/main/java/depth/board/controller/BoardController.java
@@ -4,9 +4,10 @@ import depth.board.entity.Board;
 import depth.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -27,5 +28,40 @@ public class BoardController {
     public String boardWritePro(Board board) {
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardlist());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")  // localhost:8090/board/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
     }
 }
